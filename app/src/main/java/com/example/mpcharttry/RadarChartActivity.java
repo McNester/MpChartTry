a401b79ed93903fd0d91e657564f2f7da33e9e67
McNester <seraf5@bk.ru>
2021-04-12 20:13:42 +0600

    добавил кнопки

--- app/src/main/java/com/example/mpcharttry/RadarChartActivity.java
+++ app/src/main/java/com/example/mpcharttry/RadarChartActivity.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RadarChartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.btn)
-
-
+        setContentView(R.layout.activity_radar_chart);
     }
 }
